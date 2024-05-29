c241fb48a2d91158e3a4c1bf5060fd003bdeaeba
Josef Heiss <josef.heiss@jovisco.de>
2024-05-29 19:40:12 +0200

    after section 40

--- src/main/java/com/jovisco/spring6restmvc/config/SpringSecurityConfig.java
+++ src/main/java/com/jovisco/spring6restmvc/config/SpringSecurityConfig.java
@@ -2,22 +2,29 @@ package com.jovisco.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Profile("!test")
 @Configuration
 public class SpringSecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-            .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
-            .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
-            // .httpBasic(Customizer.withDefaults())
-            // .csrf((csrf) -> csrf.ignoringRequestMatchers("/api/**"));
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> {
+                    authorize
+                        .requestMatchers("/v3/api-docs**", "/v3/api-docs/**", "/swagger-ui/**",  "/swagger-ui.html")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated();
+                })
+               .oauth2ResourceServer(httpSecurityOAuth2ResourceServerConfigurer -> {
+                     httpSecurityOAuth2ResourceServerConfigurer.jwt(Customizer.withDefaults());
+                });
 
-	    return httpSecurity.build();
+        return http.build();
     }
 
 }
